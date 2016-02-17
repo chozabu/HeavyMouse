31f32163ed80b33437b01515435f6e61933f3a75
Chozabu <chozabu@gmail.com>
2016-02-17 11:19:09 +0000

    initial commit, basic code and readme

---
+++ heavymouse.py
@@ -0,0 +1,66 @@
+import sys, time
+from pymouse import PyMouse
+
+m = PyMouse()
+
+mx, my = m.position()
+mx,my=float(mx), float(my)
+
+vx,vy=.0,.0
+
+width,height = m.screen_size()
+
+print('Press Ctrl-C to quit.')
+try:
+    while True:
+		#get current position
+        x, y = m.position()
+        x,y=float(x), float(y)
+        
+        #alter vel by user input
+        vx += float(x-mx)-vx
+        vy += float(y-my)-vy
+        
+        #drag
+        vx*=0.98
+        vy*=0.98
+        
+        #gravity
+        vy+=1.5
+        
+        #update current and previous position
+        mx=x
+        my=y
+        x+=vx
+        y+=vy
+        
+        #wrap or bounce at screen edges
+        if x < 0:
+			x+=width
+			mx+=width
+        if y < 0:
+			y+=height
+			my+=height
+        if x > width:
+			x-=width
+			mx-=width
+        if y > height:
+			#y-=height
+			#my-=height
+			y-=vy*2
+			vy=-vy
+        
+        #apply position to mouse
+        m.move(x, y)
+        
+        #debug output
+        #positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        #print(positionStr)
+        #print(vx, vy)
+        #print('\b' * len(positionStr))
+        #sys.stdout.flush()
+        
+        #100 FPS maximum?
+        time.sleep(.02)
+except KeyboardInterrupt:
+    print('\n')
---
+++ readme.md
@@ -0,0 +1,7 @@
+#HeavyMouse
+
+This is a silly program, designed to give your mouse momemtum, gravity and wrap/bounce at the screens edge.
+
+To run:
+
+    python heavymouse.py
