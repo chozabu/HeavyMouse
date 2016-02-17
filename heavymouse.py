e3050ea77e8a25927c85a24184da00cdfab22e18
Chozabu <chozabu@gmail.com>
2016-02-17 14:20:37 +0000

    remove tabs, use 4 spaces

--- heavymouse.py
+++ heavymouse.py
@@ -13,7 +13,7 @@ width,height = m.screen_size()
 print('Press Ctrl-C to quit.')
 try:
     while True:
-		#get current position
+        #get current position
         x, y = m.position()
         x,y=float(x), float(y)
         
@@ -36,19 +36,19 @@ try:
         
         #wrap or bounce at screen edges
         if x < 0:
-			x+=width
-			mx+=width
+            x+=width
+            mx+=width
         if y < 0:
-			y+=height
-			my+=height
+            y+=height
+            my+=height
         if x > width:
-			x-=width
-			mx-=width
+            x-=width
+            mx-=width
         if y > height:
-			#y-=height
-			#my-=height
-			y-=vy*2
-			vy=-vy
+            #y-=height
+            #my-=height
+            y-=vy*2
+            vy=-vy
         
         #apply position to mouse
         m.move(x, y)
