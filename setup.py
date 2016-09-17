d3f8a7a03a594ce2f6866f20be38399e2784f544
chozabu <chozabu@gmail.com>
2016-09-17 15:20:07 +0100

    bump pip version numbers

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'heavymouse',
   packages = ['heavymouse'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.3',
   description = 'Give your mouse some mass',
   author = 'Alex PB',
   author_email = 'chozabu@gmail.com',
   url = 'https://github.com/chozabu/HeavyMouse', # use the URL to the github repo
-  download_url = 'https://github.com/chozabu/HeavyMouse/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/chozabu/HeavyMouse/tarball/0.3', # I'll explain this in a second
   keywords = ['joke', 'fun', 'physics'], # arbitrary keywords
   classifiers = [],
 )
