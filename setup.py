fee92ade32a737cbcca2e747afcc334eda007ac8
chozabu <chozabu@gmail.com>
2016-09-17 15:47:22 +0100

    extra tweaks for pip

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'heavymouse',
   packages = ['heavymouse'], # this must be the same as the name above
-  version = '0.4',
+  version = '0.5',
   description = 'Give your mouse some mass',
   author = 'Alex PB',
   author_email = 'chozabu@gmail.com',
   url = 'https://github.com/chozabu/HeavyMouse', # use the URL to the github repo
-  download_url = 'https://github.com/chozabu/HeavyMouse/tarball/0.4', # I'll explain this in a second
+  download_url = 'https://github.com/chozabu/HeavyMouse/tarball/0.5', # I'll explain this in a second
   keywords = ['joke', 'fun', 'physics'], # arbitrary keywords
   classifiers = [],
 )
